3dba16f4bbf9362bfbc77cec0a30b19e70970cd1
Varna Vincent <varnamvmv@gmail.com>
2018-03-10 00:33:07 -0800

    Simulated fake processes

--- src/com/mobiledevicesimulation/Activity.java
+++ src/com/mobiledevicesimulation/Activity.java
@@ -2,7 +2,7 @@ package com.mobiledevicesimulation;
 
 public interface Activity {
 
-    public void executeinForeground(boolean isPowerSaveModeOn);
+    public void executeinForeground(boolean isPowerSaveModeOn, double power);
 
-    public void executeinBackground(boolean isPowerSaveModeOn);
+    public void executeinBackground(boolean isPowerSaveModeOn, double power);
 }
--- src/com/mobiledevicesimulation/apps/SharedDocs.java
+++ src/com/mobiledevicesimulation/apps/SharedDocs.java
@@ -4,35 +4,39 @@ import com.mobiledevicesimulation.Process;
 
 public class SharedDocs extends Process {
 
-    private boolean isPowerSaveModeOn;
+    private final double POWER_FOREGROUND = 5;
+    private final double POWER_BACKGROUND = 3;
+    private final double POWER_FOREGROUND_POWERSAVEMODE = 2.5;
+    private final double POWER_BACKGROUND_POWERSAVEMODE = 2;
 
     public SharedDocs() {
-        System.out.println("Started Shared Docs App");
+
+        setPowerForeground(POWER_FOREGROUND);
+        setPowerBackground(POWER_BACKGROUND);
+        setPowerForegroundPowerSaveMode(POWER_FOREGROUND_POWERSAVEMODE);
+        setPowerBackgroundPowerSaveMode(POWER_BACKGROUND_POWERSAVEMODE);
+        setStatus("Background");
+        System.out.println("Running Google Docs in " + getStatus());
     }
 
     @Override
-    public void executeinForeground(boolean isPowerSaveModeOn) {
+    public void executeinForeground(boolean isPowerSaveModeOn, double power) {
 
-        this.isPowerSaveModeOn = isPowerSaveModeOn;
-        // Poll for user input
-        consumePower();
+        if(hasPowerToExecute(power, POWER_FOREGROUND_POWERSAVEMODE)) {
+            System.out.print("\nGoogle Docs: Blocked for user input...");
+            consumePower(isPowerSaveModeOn);
+            displayPower(power);
+        }
     }
 
     @Override
-    public void executeinBackground(boolean isPowerSaveModeOn) {
+    public void executeinBackground(boolean isPowerSaveModeOn, double power) {
 
-        this.isPowerSaveModeOn = isPowerSaveModeOn;
-        // Network Scan Activity every 30 sec
-        // If successful, save changes and sync
-        consumePower();
-    }
-
-    @Override
-    public void consumePower() {
-
-        switch (getStatus()) {
-            case "Foreground": setPowerConsumed(isPowerSaveModeOn ? 2.5 : 6); break;
-            case "Background": setPowerConsumed(isPowerSaveModeOn ? 2 : 3); break;
+        if(hasPowerToExecute(power, POWER_BACKGROUND_POWERSAVEMODE)) {
+            System.out.print("\nGoogle Docs: Scanning for network connectivity...");
+            System.out.print("\nGoogle Docs: Saving changes....");
+            consumePower(isPowerSaveModeOn);
+            displayPower(power);
         }
     }
 }
